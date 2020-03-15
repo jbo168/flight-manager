c79915dfcee14623076fb9226b3420b7c0581684
John Long <John Long>
2020-03-15 22:25:58 +0000

    Added start of booking structure

--- src/main/java/com/flightmanager/app/controller/BookingController.java
+++ src/main/java/com/flightmanager/app/controller/BookingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BookingController {
 
-    @GetMapping(value = "/bookFlight")
+    @GetMapping(value = "flight/bookFlight")
     public String index() {
         return "bookFlight";
     }
