51d74823c83ef04526b80b31873a5fc83ea1db02
John Long <John Long>
2020-03-22 23:57:27 +0000

    Added some command pattern logic and button to flights screen

--- src/main/java/com/flightmanager/app/command/CancelFlight.java
+++ src/main/java/com/flightmanager/app/command/CancelFlight.java
@@ -1,9 +1,21 @@
 package com.flightmanager.app.command;
 
+import com.flightmanager.app.model.Booking;
+import com.flightmanager.app.model.Flight;
+
 /**
  * @author: John Long
  * @create: 17-Mar-2020
  **/
 
-public class CancelFlight {
+public class CancelFlight implements FlightBooking{
+    private Booking booking;
+
+    public CancelFlight (Booking booking){
+        this.booking = booking;
+    }
+
+    public void execute() {
+        //TODO reduce number of seats booked on a particular flight by 1
+    }
 }
