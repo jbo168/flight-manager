73a7952bb2c9d4e0af59aec3d054d9e393b05fe4
NarrowOne <comkilbane@gmail.com>
2020-03-29 21:03:51 +0100

    Added userID and email

--- src/main/java/com/flightmanager/app/model/BookingData.java
+++ src/main/java/com/flightmanager/app/model/BookingData.java
@@ -4,16 +4,34 @@ package com.flightmanager.app.model;
 public class BookingData {
     //luggage, address, extra request, class
     private int luggage;
+    private int userID;
+    private String userEmail;
     private String address;
     private String ticketClass;
     private String extraRequest;
 
-    public int isLuggage() {
+    public int getLuggage() {
         return luggage;
     }
 
     public void setLuggage(int luggage) {
         this.luggage = luggage;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
     public String getAddress() {
