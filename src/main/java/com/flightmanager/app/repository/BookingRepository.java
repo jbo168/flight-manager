da6c84580318699927235d87cba2b79ab957357f
John Long <John Long>
2020-04-04 02:33:19 +0100

    Added cancel booking controller method, booking id not being passed in model

--- src/main/java/com/flightmanager/app/repository/BookingRepository.java
+++ src/main/java/com/flightmanager/app/repository/BookingRepository.java
@@ -3,5 +3,10 @@ package com.flightmanager.app.repository;
 import com.flightmanager.app.model.Booking;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * @author: John Long
+ * @create: 24-Feb-2020
+ **/
+
 public interface BookingRepository extends CrudRepository<Booking,Integer> {
 }
