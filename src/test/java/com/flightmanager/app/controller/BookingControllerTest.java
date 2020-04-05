ad083a5cbac7b0adbd7703c102da25d4089dfbbc
John Long <John Long>
2020-04-06 00:05:32 +0100

    Added some tests

--- src/test/java/com/flightmanager/app/controller/BookingControllerTest.java
+++ src/test/java/com/flightmanager/app/controller/BookingControllerTest.java
@@ -1,12 +1,19 @@
 package com.flightmanager.app.controller;
 
+import com.flightmanager.app.delegate.CardDelegate;
+import com.flightmanager.app.model.Card;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingControllerTest {
     @Test
-    void test1(){
+    void processPaymentTest() {
+        Card card = new Card();
+        card.setCardNumber("4564456445644564");
+        card.setCsv("123");
+        card.setExpDate("10/2020");
 
+        card.setAuth(new CardDelegate(card));
     }
 }
--- src/test/java/com/flightmanager/app/controller/FlightControllerTest.java
+++ src/test/java/com/flightmanager/app/controller/FlightControllerTest.java
@@ -1,12 +1,64 @@
 package com.flightmanager.app.controller;
 
+import com.flightmanager.app.dao.FlightDAO;
+import com.flightmanager.app.model.Flight;
+import com.flightmanager.app.service.FlightService;
+import com.flightmanager.app.serviceImpl.FlightServiceImpl;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.mockito.Mockito.*;
+
+
+import javax.inject.Inject;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@RunWith(MockitoJUnitRunner.class)
 class FlightControllerTest {
+//    @Mock
+//    private FlightDAO flightDAO;
+
+    private FlightService flightService;
+
+
+    private Flight createFlight(){
+        Flight flight = new Flight();
+        flight.setFlightId(0);
+        flight.setAirline("Aer Lingus");
+        flight.setRouteFrom("Dublin");
+        flight.setRouteTo("Dubai");
+        flight.setDate("21/07/2020");
+        flight.setCost(1000);
+        flight.setTickets(250);
+        flight.setStatus("Pending Confirmation");
+
+        return flight;
+    }
+
     @Test
-    void test2(){
+    void saveFlightTest(){
+        Flight flight = createFlight();
+//        flightService.save(flight);
+
+
+//        verify(flightDAO).save(new Flight());
+//        Optional<Flight> flightData = flightService.findById(0);
+//        Flight testFlightData = flightData.get();
+
+        assertEquals("Aer Lingus", flight.getAirline());
+        assertEquals("Dublin", flight.getRouteFrom());
+        assertEquals("Dubai", flight.getRouteTo());
+        assertEquals("21/07/2020", flight.getDate());
+        assertEquals(1000, flight.getCost());
+        assertEquals(250, flight.getTickets());
+        assertEquals("Pending Confirmation", flight.getStatus());
 
     }
 }
